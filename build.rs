b48e0bdd5b54e9f107f10493aa1453b5e4bb85ba
Christian Brendlin <ch@brendlinonline.de>
2024-10-18 19:35:20 +0200

    build proccess

---
+++ build.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("cargo:rustc-link-search=native={}", std::env::var("ZLIB_LIB_DIR").unwrap());
+    println!("cargo:rustc-link-search=native={}", std::env::var("CFITSIO_LIB_DIR").unwrap());
+    println!("cargo:rustc-link-lib=static=z");
+    println!("cargo:rustc-link-lib=static=fitsio");
+    println!("cargo:rerun-if-env-changed=ZLIB_LIB_DIR");
+    println!("cargo:rerun-if-env-changed=CFITSIO_LIB_DIR");
+}
