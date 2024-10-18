e18ce69ea3ed0e7708c9999bf42a9be864e551bd
Christian Brendlin <ch@brendlinonline.de>
2024-10-18 19:31:07 +0200

    Build Process

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+use fitsio::FitsFile;
+
+pub fn open_fits_file(filename: &str) -> FitsFile {
+    FitsFile::open(filename).unwrap()
+}
+
 #[no_mangle]
 pub extern "C" fn add(left: u64, right: u64) -> u64 {
     left + right
